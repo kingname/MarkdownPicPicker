d9c7701a9c65e86d9e5552f3d21c3c80051ec26d
kingname <greensouth@foxmail.com>
2016-06-05 17:02:29 +0800

    [201606051702] MarkdownPicPicker v0.2
    [Description]
    this version has refactor the config part so
    that you can config the program outside the code

--- MarkdownPicPicker.py
+++ MarkdownPicPicker.py
@@ -1,57 +1,93 @@
 from PIL import ImageGrab, ImageFile
-from qiniu import Auth, put_file
-import pythoncom
-import pyHook
+from configparser import ConfigParser
+from uploader.QiniuUploader import QiniuUploader
 import time
 import os
+try:
+    import pythoncom
+except ImportError:
+    print('maybe you have not install pywin32 or not in windows system.')
+    pythoncom = None
+try:
+    import pyHook
+except ImportError:
+    print('maybe you have not install pyHook.')
+    pyHook = None
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 __version__ = 0.1
 __author__ = 'kingname'
 
+
 class MarkrdownPicPicker(object):
 
-    METHOD = 'bat'
-    PICTURE_FOLDER = 'pic'
-    PICTURE_SUFFIX = 'png'
-    ACCESS_KEY = 'Q6sS422O05Aw345235M3FqCcCpF36tqvyQ75Zvzw'
-    SECRET_KEY = '6QtAqqTxoSxZP-2uo23455X2CCmoOaB2aLObM'
-    CONTAINER_NAME = 'picturebed'
-    URL = 'http://7sbpmp.com1.z0.glb.clouddn.com/{}'
-
-    SHORT_KEY_ONE = 'Lwin'
-    SHORT_KEY_TWO = 'C'
+    CONFIG_PATH = 'config.ini'
 
     def __init__(self):
-        self.upload_handler = None
+
+        self.method = ''
+        self.picture_folder = ''
+        self.picture_suffix = ''
+        self.picture_bed = ''
+        self.short_key_one = ''
+        self.short_key_two = ''
         self.key_one = False
         self.key_two = False
+        self.uploader = None
+
         self.init_environment()
-        '''
-        there are some bugs in pyHook, if a windows name is Unicode, it will make
-        python crash. wait until pyHook is fixed up.
-        '''
-        if self.METHOD == 'bat':
+
+        if self.method == 'bat':
             self.upload_picture()
-        elif self.METHOD == 'pyHook':
-            self.keyboard_listen
+        elif self.method == 'global_listen':
+            self.keyboard_listen()
 
     def init_environment(self):
-        if not os.path.exists(self.PICTURE_FOLDER):
-            os.makedirs(self.PICTURE_FOLDER)
+        self.read_config()
+        if not self.method \
+                or not self.picture_folder \
+                or not self.picture_suffix \
+                or not self.picture_bed:
+            print('there must be something wrong in config, please check.')
+            exit()
+        if not os.path.exists(self.picture_folder):
+            os.makedirs(self.picture_folder)
 
-        self.upload_handler = Auth(self.ACCESS_KEY, self.SECRET_KEY)
+    def read_config(self):
+        configs = ConfigParser()
+        if not os.path.exists(self.CONFIG_PATH):
+            print('can not find the config.ini, exit')
+            exit()
+        configs.read(self.CONFIG_PATH)
+        self.method = configs['basic'].get('run_method', '')
+        self.picture_folder = configs['basic'].get('picture_folder', '')
+        self.picture_suffix = configs['basic'].get('picture_suffix', '')
+        self.picture_bed = configs['basic'].get('picture_bed', '')
+
+        if self.picture_bed:
+            self.uploader = QiniuUploader(configs['qiniu'])
+
+        if self.method == 'global_listen':
+            self.short_key_one = configs['global_listen']['short_key_one']
+            self.short_key_two = configs['global_listen']['short_key_two']
 
     def keyboard_listen(self):
+        if not pythoncom or not pyHook:
+            print('as pythoncom or pyHook is not exists, please use bat method.')
+            exit()
+        if not self.short_key_one or not self.short_key_two:
+            print('there must be something wrong in the config, please check.')
+            exit()
         hm = pyHook.HookManager()
         hm.KeyDown = self.keyboard_event
         hm.HookKeyboard()
         pythoncom.PumpMessages()
 
     def keyboard_event(self, event):
-        if event.Key == self.SHORT_KEY_ONE and not self.key_one:
+        if event.Key == self.short_key_one and not self.key_one:
             self.key_one = True
-        elif event.Key == self.SHORT_KEY_TWO and not self.key_two:
+        elif event.Key == self.short_key_two and not self.key_two:
             self.key_two = True
         if self.key_one and self.key_two:
             self.upload_picture()
@@ -64,34 +100,24 @@ class MarkrdownPicPicker(object):
         if not picture_path:
             return False
         else:
-            self.upload(picture_path, picture_name)
-            self.write_markdown_picture_url(picture_name)
+            self.uploader.upload(picture_path, picture_name)
+            self.uploader.write_markdown_picture_url(picture_name)
+            return True
 
     def save_picture(self):
         date_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
-        picture_name = date_time + '.' + self.PICTURE_SUFFIX
-        picture_path = os.path.join(self.PICTURE_FOLDER, picture_name)
+        picture_name = date_time + '.' + self.picture_suffix
+        picture_path = os.path.join(self.picture_folder, picture_name)
         try:
             picture_data = ImageGrab.grabclipboard()
             if picture_data:
-                picture_data.save(picture_path, self.PICTURE_SUFFIX)
-                return (picture_path, picture_name)
+                picture_data.save(picture_path, self.picture_suffix)
+                return picture_path, picture_name
             else:
                 print('there is no picture in clipboard!')
         except Exception as e:
             print('get picture from clipboard error because: {}'.format(e))
-        return ('', '')
-
-    def upload(self, picture_path, picture_name):
-        if self.upload_handler:
-            token = self.upload_handler.upload_token(self.CONTAINER_NAME, picture_name, 3600)
-            info = put_file(token, picture_name, picture_path)
-            print(info)
-
-    def write_markdown_picture_url(self, picture_name):
-        markdown_picture_url = '![]({})'.format(self.URL.format(picture_name))
-        command = 'echo {} | clip'.format(markdown_picture_url)
-        os.system(command)
+        return '', ''
 
 if __name__ == '__main__':
     MarkrdownPicPicker()
---
+++ uploader/QiniuUploader.py
@@ -0,0 +1,21 @@
+from qiniu import Auth, put_file
+import os
+
+
+class QiniuUploader(object):
+    def __init__(self, config_info):
+        self.upload_handler = None
+        self.url = config_info['url']
+        self.container_name = config_info.get('container_name')
+        self.upload_handler = Auth(config_info.get('access_key'), config_info.get('secret_key'))
+
+    def upload(self, picture_path, picture_name):
+        if self.upload_handler:
+            token = self.upload_handler.upload_token(self.container_name, picture_name, 3600)
+            info = put_file(token, picture_name, picture_path)
+            print(info)
+
+    def write_markdown_picture_url(self, picture_name):
+        markdown_picture_url = '![]({})'.format(self.url.format(picture_name))
+        command = 'echo {} | clip'.format(markdown_picture_url)
+        os.system(command)
