c9ff250ccb35b87cf2465be2116d365789ae2832
kingname <greensouth@foxmail.com>
2017-04-14 09:11:20 +0800

    add conf.py for read the doc.

---
+++ conf.py
@@ -0,0 +1,11 @@
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pywin32']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
